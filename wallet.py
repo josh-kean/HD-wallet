2a5e370cc776859a3cc596b844fef181b7c97cad
jkean <joshkn@protonmail.com>
2020-04-06 11:05:24 -0400

    created function to create a new branch

--- wallet.py
+++ wallet.py
@@ -4,18 +4,43 @@ import hmac as h
 import mnemonic_phrase
 import hashlib
 
-def mnemonic_gen(phrase=None): #considering returning an iterable object so the binary seed and the word list can be returned from this function
-    seed_and_phrase = mnemonic_phrase.new_seed(phrase)
-    return seed_and_phrase
+class Wallet:
+    def __init__(self, mnemonic_phrase = None):
+        self.mnemonic_phrase = mnemonic_phrase
+        self.initial_seed = None
+        self.public_keys = {}
+        self.private_keys = {} #dictionary key = branch, dictionary value = array [key, chaincode]
 
-def master_key(b_seq):
-    b_seq = b_seq.encode('utf-8')
-    b = h.new(b'Bitcoin Seed', msg=b_seq, digestmod=hashlib.sha512).digest()
-    I = format(int.from_bytes(b, byteorder='big'), '0512b')
-    iL, iR = split_I(I)
-    m_key = parse256(int(iL,16)) #parse256 needsto be input as byte sequence
-    c_code = iR
-    return [m_key, c_code]
+    def mnemonic_gen(self): #considering returning an iterable object so the binary seed and the word list can be returned from this function
+        seed_and_phrase = mnemonic_phrase.new_seed(self.mnemonic_phrase)
+        self.initial_seed = seed_and_phrase[0]
+        if not self.mnemonic_phrase: self.mnemonic_phrase = seed_and_phrase[1]
+
+    def master_key(self):
+        b_seq = self.initial_seed.encode('utf-8')
+        b = h.new(b'Bitcoin Seed', msg=b_seq, digestmod='SHA512').digest()
+        I = format(int.from_bytes(b, byteorder='big'), '0512b')
+        iL, iR = split_I(I)
+        self.private_keys['master']= [parse256(int(iL,16)), iR] 
+
+    def private_child_key(self, branch, parent_index, index, hardened = False):
+        if hardened: index+=(2**31)
+        key = self.private_keys[branch][parent_index][0]
+        chain_code = self.private_keys[branch][parent_index][1]
+        return CKDpriv(key, chain_code, index)
+
+    def public_child_key(self, branch, parent_index, index):
+        key = self.public_keys[branch][parent_index][0]
+        chain_code = self.public_keys[branch][parent_index][1]
+        return CKDpub(key, chain_code, index)
+
+    def create_branch(parent_branch, parent_index, new_branch, key_indicies, branch_type):
+        if branch_type == 'private':
+            self.private_keys[new_branch] = [self.private_child_key(parent_branch, parent_index, i) for i in key_indixies]
+        if branch_type == 'public':
+            self.public_keys[new_branch] = [self.public_child_key(parent_branch, parent_index, i) for i in key_indixies]
+
+
 
 if __name__ == '__main__':
     bseq = mnemonic_gen()
